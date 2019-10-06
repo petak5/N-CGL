58a5189bb1ebac878e1a334647a91037507424c9
Dan Sirbu <dsirb066@gmail.com>
2019-10-06 09:10:30 -0400

    Make window take the full terminal size

--- main.c
+++ main.c
@@ -3,14 +3,11 @@
 int main(void)
 {
     initscr();
+	cbreak(); //disable line buffering so stuff is draw even without \n
 	noecho();
+	clear();
 
-	int height, width, start_y, start_x;
-	height = 10;
-	width = 20;
-	start_y = start_x = 10;
-
-	WINDOW * win = newwin(height, width, start_y, start_x);
+	WINDOW * win = newwin(LINES, COLS, 0, 0); //make the window full screen
 	refresh();
 
 	box(win, 0, '*');
