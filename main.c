5e15bd4dab1a216a525df5f79c194048b7b85761
petak5 <peterurgos12345@gmail.com>
2019-10-08 07:27:20 +0200

    Rendering in the game loop. (no logic yet)

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
+#include <unistd.h>
 #include <ncurses.h>
+
+void clear_grid(char (* grid)[LINES][COLS]);
+void print_grid(char (* grid)[LINES][COLS], WINDOW * win);
+
 
 int main(void)
 {
@@ -8,16 +13,41 @@ int main(void)
 	clear();
 
 	WINDOW * win = newwin(LINES, COLS, 0, 0); //make the window full screen
-	refresh();
+	refresh();	
 
-	box(win, 0, '*');
-	mvwprintw(win, 1, 1, "Hi there :)");
-	wrefresh(win);
-	
-	getch();
+	char grid[LINES][COLS];
 
+	clear_grid(&grid);
+
+	grid[3][5] = ' ';
+
+	while(TRUE)
+	{
+		print_grid(&grid, win);
+		wrefresh(win);
+		sleep(2);	// 2 seconds
+	}
 
 	endwin();
 
     return 0;
+}
+
+void clear_grid(char (* grid)[LINES][COLS])
+{
+	(* grid)[0][0] = 0;
+	for (int i = 0; i < LINES; i++) {
+		for (int j = 0; j < COLS; j++) {
+			(* grid)[i][j] = '*';
+		}
+	}
+}
+
+void print_grid(char (* grid)[LINES][COLS], WINDOW * win)
+{
+	for (int i = 0; i < LINES; i++) {
+		for (int j = 0; j < COLS; j++) {
+			mvwprintw(win, i, j, "%c", (* grid)[i][j]);
+		}
+	}
 }
