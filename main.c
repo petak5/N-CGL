b90a6bf8ab6ef990b64a10204575b68df682132f
Dan Sirbu <dsirb066@gmail.com>
2019-10-16 19:09:51 -0400

    Used auto formatting on main.c

--- main.c
+++ main.c
@@ -6,17 +6,18 @@
 #include <sys/time.h>
 #include <string.h>
 
-void exit_handler() {
+void exit_handler()
+{
 	endwin();
 }
 
 void processInput(char (*grid)[LINES][COLS], WINDOW *win);
-void clear_grid(char (* grid)[LINES][COLS]);
-void iterate_grid(char (* grid)[LINES][COLS]);
-void print_grid(char (* grid)[LINES][COLS], WINDOW * win);
-int neighbours_count(char (* grid)[LINES][COLS], int y, int x);
+void clear_grid(char (*grid)[LINES][COLS]);
+void iterate_grid(char (*grid)[LINES][COLS]);
+void print_grid(char (*grid)[LINES][COLS], WINDOW *win);
+int neighbours_count(char (*grid)[LINES][COLS], int y, int x);
 
-FILE* logFile;
+FILE *logFile;
 
 #define ALIVE_CHAR '*'
 #define DEAD_CHAR ' '
@@ -27,7 +28,7 @@ int paused;
 
 int main(void)
 {
-    initscr();
+	initscr();
 	cbreak(); //disable line buffering so stuff is draw even without \n
 	noecho();
 	clear();
@@ -35,24 +36,23 @@ int main(void)
 
 	signal(SIGSEGV, exit_handler);
 
-	WINDOW * win = newwin(LINES, COLS, 0, 0); //make the window full screen
-	nodelay(win, TRUE); //make input non-blocking
-	keypad(win, TRUE); //Needs to be set to true or mouse events just come in as garbage
+	WINDOW *win = newwin(LINES, COLS, 0, 0); //make the window full screen
+	nodelay(win, TRUE);						 //make input non-blocking
+	keypad(win, TRUE);						 //Needs to be set to true or mouse events just come in as garbage
 	mousemask(BUTTON1_RELEASED | REPORT_MOUSE_POSITION, NULL);
 	mouseinterval(0);
 	refresh();
 
 	logFile = fopen("log.txt", "w");
-	if (logFile == NULL) 
-    { 
+	if (logFile == NULL)
+	{
 		endwin();
-        exit(-1);
-    }
+		exit(-1);
+	}
 
 	char grid[LINES][COLS];
 
 	clear_grid(&grid);
-
 
 	/* A glider
 	grid[10][10] = ALIVE_CHAR;
@@ -65,7 +65,7 @@ int main(void)
 	struct timeval lastUpdate, currentTime;
 	memset(&lastUpdate, 0, sizeof(lastUpdate));
 
-	while(TRUE)
+	while (TRUE)
 	{
 		processInput(&grid, win);
 		print_grid(&grid, win);
@@ -73,8 +73,9 @@ int main(void)
 
 		gettimeofday(&currentTime, NULL);
 		float millisDiff = (currentTime.tv_sec - lastUpdate.tv_sec) * 1000.0f + (currentTime.tv_usec - lastUpdate.tv_usec) / 1000.0f;
-		
-		if(millisDiff > simulation_interval_millis && !paused) {
+
+		if (millisDiff > simulation_interval_millis && !paused)
+		{
 			memcpy(&lastUpdate, &currentTime, sizeof(currentTime));
 			iterate_grid(&grid);
 
@@ -86,134 +87,172 @@ int main(void)
 		usleep(16000); //cap it at 60 fps (1000 millis / 60 = 16.66 ms)
 	}
 
-    return 0;
+	return 0;
 }
 
-void processInput(char (*grid)[LINES][COLS], WINDOW *win) {
+void processInput(char (*grid)[LINES][COLS], WINDOW *win)
+{
 	int ch = wgetch(win);
-	if(ch == KEY_MOUSE) {
+	if (ch == KEY_MOUSE)
+	{
 		MEVENT mouseEvent;
-		if(getmouse(&mouseEvent) == OK) {
+		if (getmouse(&mouseEvent) == OK)
+		{
 			fprintf(logFile, "Mouse (%d, %d, %d)\n", mouseEvent.x, mouseEvent.y, mouseEvent.z);
 			fflush(logFile);
 
-			
-			char *cell = &(* grid)[mouseEvent.y][mouseEvent.x];
+			char *cell = &(*grid)[mouseEvent.y][mouseEvent.x];
 			*cell = *cell == ALIVE_CHAR ? DEAD_CHAR : ALIVE_CHAR;
 		}
-	} else if(ch == ' ') {
+	}
+	else if (ch == ' ')
+	{
 		paused = !paused;
-	} else if(ch == '-') {
-		if(simulation_interval_millis < 1) { //if it's 0, then doubling it is useless
+	}
+	else if (ch == '-')
+	{
+		if (simulation_interval_millis < 1)
+		{ //if it's 0, then doubling it is useless
 			simulation_interval_millis = 1;
-		} else {
+		}
+		else
+		{
 			simulation_interval_millis *= 2; //Slow it down
 		}
-	} else if(ch == '=') {
+	}
+	else if (ch == '=')
+	{
 		simulation_interval_millis /= 2; //Speed it up
 	}
 }
 
-void clear_grid(char (* grid)[LINES][COLS])
+void clear_grid(char (*grid)[LINES][COLS])
 {
-	(* grid)[0][0] = 0;
-	for (int i = 0; i < LINES; i++) {
-		for (int j = 0; j < COLS; j++) {
-			(* grid)[i][j] = DEAD_CHAR;
+	(*grid)[0][0] = 0;
+	for (int i = 0; i < LINES; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			(*grid)[i][j] = DEAD_CHAR;
 		}
 	}
 }
 
-void iterate_grid(char (* grid)[LINES][COLS])
+void iterate_grid(char (*grid)[LINES][COLS])
 {
 	// An array consisting of the number of alive cells for each cell
 	char temp_count[LINES][COLS];
 
 	// Get the alive neighbour cells counts populated into the array
-	for (int i = 0; i < LINES; i++) {
-		for (int j = 0; j < COLS; j++) {
+	for (int i = 0; i < LINES; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
 			temp_count[i][j] = neighbours_count(grid, i, j);
 		}
 	}
 
 	// Apply the rules and update the board
-	for (int i = 0; i < LINES; i++) {
-		for (int j = 0; j < COLS; j++) {
-			if (temp_count[i][j] < 2) {
-				(* grid)[i][j] = DEAD_CHAR;
+	for (int i = 0; i < LINES; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (temp_count[i][j] < 2)
+			{
+				(*grid)[i][j] = DEAD_CHAR;
 			}
-			if (temp_count[i][j] > 3) {
-				(* grid)[i][j] = DEAD_CHAR;
+			if (temp_count[i][j] > 3)
+			{
+				(*grid)[i][j] = DEAD_CHAR;
 			}
-			if (temp_count[i][j] == 3) {
-				(* grid)[i][j] = ALIVE_CHAR;
+			if (temp_count[i][j] == 3)
+			{
+				(*grid)[i][j] = ALIVE_CHAR;
 			}
 		}
 	}
 }
 
-void print_grid(char (* grid)[LINES][COLS], WINDOW * win)
+void print_grid(char (*grid)[LINES][COLS], WINDOW *win)
 {
-	for (int i = 0; i < LINES; i++) {
-		for (int j = 0; j < COLS; j++) {
-			mvwprintw(win, i, j, "%c", (* grid)[i][j]);
+	for (int i = 0; i < LINES; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			mvwprintw(win, i, j, "%c", (*grid)[i][j]);
 		}
 	}
 }
 
 /* Returns the number of alive cels next to [x, y] cell */
-int neighbours_count(char (* grid)[LINES][COLS], int y, int x)
+int neighbours_count(char (*grid)[LINES][COLS], int y, int x)
 {
 	int count = 0;
 
 	/* Horizontal */
 	// Left
-	if (x > 0) {
-		if ((* grid)[y][x - 1] == ALIVE_CHAR) {
+	if (x > 0)
+	{
+		if ((*grid)[y][x - 1] == ALIVE_CHAR)
+		{
 			count++;
 		}
 	}
 	// Right
-	if (x < COLS) {
-		if ((* grid)[y][x + 1] == ALIVE_CHAR) {
+	if (x < COLS)
+	{
+		if ((*grid)[y][x + 1] == ALIVE_CHAR)
+		{
 			count++;
 		}
 	}
 	/* Vertical */
 	// Top
-	if (y > 0) {
-		if ((* grid)[y - 1][x] == ALIVE_CHAR) {
+	if (y > 0)
+	{
+		if ((*grid)[y - 1][x] == ALIVE_CHAR)
+		{
 			count++;
 		}
 	}
 	// Bottom
-	if (y < LINES) {
-		if ((* grid)[y + 1][x] == ALIVE_CHAR) {
+	if (y < LINES)
+	{
+		if ((*grid)[y + 1][x] == ALIVE_CHAR)
+		{
 			count++;
 		}
 	}
 	/* Diagonal */
 	// Top Left
-	if ((x > 0) && (y > 0)) {
-		if ((* grid)[y - 1][x - 1] == ALIVE_CHAR) {
+	if ((x > 0) && (y > 0))
+	{
+		if ((*grid)[y - 1][x - 1] == ALIVE_CHAR)
+		{
 			count++;
 		}
 	}
 	// Top Right
-	if ((x < COLS) && (y > 0)) {
-		if ((* grid)[y - 1][x + 1] == ALIVE_CHAR) {
+	if ((x < COLS) && (y > 0))
+	{
+		if ((*grid)[y - 1][x + 1] == ALIVE_CHAR)
+		{
 			count++;
 		}
 	}
 	// Bottom Left
-	if ((x > 0) && (y <LINES)) {
-		if ((* grid)[y + 1][x - 1] == ALIVE_CHAR) {
+	if ((x > 0) && (y < LINES))
+	{
+		if ((*grid)[y + 1][x - 1] == ALIVE_CHAR)
+		{
 			count++;
 		}
 	}
 	// Bottm Right
-	if ((x < COLS) && (y < LINES)) {
-		if ((* grid)[y + 1][x + 1] == ALIVE_CHAR) {
+	if ((x < COLS) && (y < LINES))
+	{
+		if ((*grid)[y + 1][x + 1] == ALIVE_CHAR)
+		{
 			count++;
 		}
 	}
